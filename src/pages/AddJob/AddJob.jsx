4e066849aa60f6090ba1b73bf59f72b392f55e12
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-12-14 19:32:16 +0600

    Frontend completed

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const AddJob = () => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        watch,
+        formState: { errors },
+    } = useForm();
+
+    const axiosSecure = useAxiosSecure();
+    const onSubmit = async (data) => {
+        console.log(data)
+        const result = await axiosSecure.post('/post_job', data);
+        if (result.data.insertedId) {
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Job description has been added.",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+        reset();
+    };
+
+
+
     return (
         <div className='min-h-screen'>
             <Helmet>
@@ -11,80 +39,87 @@ const AddJob = () => {
                 <p className='text-center text-4xl font-semibold'>Create A Job</p>
             </div>
             <div className="py-8 px-4 mx-auto w-4/5 lg:py-16 border rounded-lg my-10">
-                <form  action="#">
+                <form onSubmit={handleSubmit(onSubmit)} action="#">
                     <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
                         {/* first row  */}
+                        {/* company name  */}
                         <div>
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Name</label>
-                            <input type="email" name="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Name" required=""  />
+                            <input type="text" {...register("company")} name='company' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Company Name" required="" />
                         </div>
+                        {/* company website  */}
                         <div>
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Website</label>
-                            <input type="email" name="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Website Link" required=""  />
+                            <input type="text" {...register("website")} name="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Website Link" required="" />
                         </div>
-                        
+
                         {/* second row  */}
+                        {/* job title  */}
                         <div className="sm:col-span-2">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Title</label>
-                            <input type="text" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Title" required="" />
+                            <input type="text" name="title" {...register("title")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Job Title" required="" />
                         </div>
 
                         {/* third row  */}
+                        {/* Qualification  */}
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Name</label>
-                            <input type="email" name="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Name" required="" />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Minimum Qualification</label>
+                            <input type="text" name="qualification" {...register("qualification")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Minimum Qualification" required="" />
                         </div>
+                        {/* Experience level  */}
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Website</label>
-                            <input type="email" name="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Website Link" required="" />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Experience Level</label>
+                            <input type="text" name="level" {...register("level")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Experience Level" required="" />
                         </div>
 
                         {/* fourth row  */}
+                        {/* Experience Year  */}
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Name</label>
-                            <input type="email" name="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Name" required="" />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Experience Year</label>
+                            <input type="text" name="year" {...register("year")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Years of experience" required="" />
                         </div>
+                        {/* Location  */}
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Website</label>
-                            <input type="email" name="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Website Link" required="" />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
+                            <input type="text" name="location" {...register("location")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Job Location" required="" />
                         </div>
 
                         {/* fifth row  */}
+                        {/* Application Deadline  */}
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Name</label>
-                            <input type="email" name="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Name" required="" />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Application Deadline</label>
+                            <input type="text" name="deadline" {...register("deadline")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Application Deadline" required="" />
                         </div>
+
+                        {/* Salary Range  */}
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Website</label>
-                            <input type="email" name="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Website Link" required="" />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Salary Range</label>
+                            <input type="text" name="salary" {...register("salary")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Salary Range" required="" />
                         </div>
 
+                        {/* Job Description  */}
                         <div className="sm:col-span-2">
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">image url</label>
-                            <input type="text" name="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter image url" required="" />
-                        </div>
-
-                        <div className="sm:col-span-2">
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Document url</label>
-                            <input type="text" name="doc_url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Document Drive Link" required="" />
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Description</label>
+                            <textarea name="description" {...register("description")} rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Job description here"></textarea>
                         </div>
 
 
-
-
-
-
+                        {/* Job Requirement  */}
                         <div className="sm:col-span-2">
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Short Description</label>
-                            <textarea name="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your short description here"></textarea>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Requirement</label>
+                            <textarea name="description" {...register("requirement")} rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Job requirements here"></textarea>
+                        </div>
+
+                        {/* Job Responsibilities  */}
+                        <div className="sm:col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Responsibilities</label>
+                            <textarea name="responsibility" {...register("responsibility")} rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Job responsibilities here"></textarea>
                         </div>
 
 
                     </div>
                     <div className='flex justify-end'>
-                        <button type="submit" className="flex justify-end btn-primary items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-[#338573] rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                            Post Job
-                        </button>
+                        <input type="submit" className='btn btn-success text-white bg-green-700 mt-5' value="Post Job" />
                     </div>
                 </form>
             </div>
--- src/pages/Home/PopularSection.jsx
+++ src/pages/Home/PopularSection.jsx
@@ -1,76 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { axiosPublic } from '../../hooks/useAxiosPublic';
+import { useQuery } from "@tanstack/react-query";
 
 const PopularSection = () => {
+
+    const { data: jobs = [] } = useQuery({
+        queryKey: ['jobs'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/jobs');
+            return res.data;
+        }
+    })
+
+    console.log(jobs)
+
     return (
         <>
             <p className='text-4xl font-semibold text-center mt-10'>All Popular Listed jobs</p>
 
-            <div className='pt-10 space-y-5'>
+            {
+                jobs?.map(job => <div key={job._id} className='pt-10 space-y-5'>
 
-                <div className="container space-y-5 mx-auto grid lg:grid-cols-7 grid-rows-1 items-center p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div className="container space-y-5 mx-auto grid lg:grid-cols-7 grid-rows-1 items-center p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
-                    <div className='text-white bg-[#338573] col-span-1 rounded- text-6xl font-bold text-center p-5 rounded-full'>f</div>
 
-                    <div className='col-span-5 lg:mx-10'>
-                        <h5 className="mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <div className='col-span-6 lg:mx-10'>
+                            <h5 className="mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{job.title}</h5>
+                            <h5 className="mb-2  text-sm font-bold tracking-tight text-gray-900 dark:text-white">{job.company}</h5>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">{job.description}</p>
+                        </div>
+
+                        <Link to="/jobDetails" className="btn text-white bg-[#338573] col-span-1">View Details</Link>
                     </div>
 
-                    <Link to="/jobDetails" className="btn text-white bg-[#338573] col-span-1">View Details</Link>
-                </div>
 
+                </div>)
+            }
 
-            </div>
-            <div className='pt-10 space-y-5'>
-
-                <div className="container space-y-5 mx-auto grid lg:grid-cols-7 grid-rows-1 items-center p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-
-                    <div className='text-white bg-[#338573] col-span-1 rounded- text-6xl font-bold text-center p-5 rounded-full'>f</div>
-
-                    <div className='col-span-5 lg:mx-10'>
-                        <h5 className="mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-
-                    <Link className="btn text-white bg-[#338573] col-span-1">View Details</Link>
-                </div>
-
-
-            </div>
-            <div className='pt-10 space-y-5'>
-
-                <div className="container space-y-5 mx-auto grid lg:grid-cols-7 grid-rows-1 items-center p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-
-                    <div className='text-white bg-[#338573] col-span-1 rounded- text-6xl font-bold text-center p-5 rounded-full'>f</div>
-
-                    <div className='col-span-5 lg:mx-10'>
-                        <h5 className="mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-
-                    <Link className="btn text-white bg-[#338573] col-span-1">View Details</Link>
-                </div>
-
-
-            </div>
-            <div className='pt-10 space-y-5'>
-
-                <div className="container space-y-5 mx-auto grid lg:grid-cols-7 grid-rows-1 items-center p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-
-                    <div className='text-white bg-[#338573] col-span-1 rounded- text-6xl font-bold text-center p-5 rounded-full'>f</div>
-
-                    <div className='col-span-5 lg:mx-10'>
-                        <h5 className="mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-
-                    <Link className="btn text-white bg-[#338573] col-span-1">View Details</Link>
-                </div>
-
-
-            </div>
             <div className='flex justify-center py-5'>
                 <Link className="btn text-white bg-[#338573]">View more<FaArrowRightFromBracket></FaArrowRightFromBracket></Link>
             </div>
